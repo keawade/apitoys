176c0c95d1ac10abb1e1ca4c7463d282ad4d1f96
keawade <keawade@gmail.com>
2015-07-01 14:48:29 -0500

    Basic Github API data grab implemented.

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    http = require('http');
+
+var app = express();
+app.use(express.static('public'));
+
+app.get('/', function(req, res, next) {
+    res.send('index.html');
+});
+
+var server = app.listen(8001, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("[github-toy] listening at http://%s%s", host, port);
+});
