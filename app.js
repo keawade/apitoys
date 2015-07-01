26af25e788e738ce0998c405bdeb353afd9e352b
keawade <keawade@gmail.com>
2015-07-01 15:27:17 -0500

    temp commit

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-var express = require('express'),
-    http = require('http');
+var express = require('express');
+var http = require('http');
+var twitter = require('twitter');
+var github = require('github');
+var config = require('./config');
 
 var app = express();
 app.use(express.static('public'));
 
 app.get('/', function(req, res, next) {
     res.send('index.html');
+});
+
+var twit = new twitter({
+    consumer_key: config.consumerKey,
+    consumer_secret: config.consumerSecret,
+    access_token_key: config.accessTokenKey,
+    access_token_secret: config.accessTokenSecret
+});
+
+twit.stream('filter', { track: 'cat' }, function (stream) {
+    stream.on('data', function (data) {
+        console.log('Tweet received from ' + data.user.screen_name + ': "' + data.text + '"');
+    });
 });
 
 var server = app.listen(8001, function () {
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumerKey: '{CONSUMER_KEY}',
+    consumerSecret: '{CONSUMER_SECRET}',
+    accessTokenKey: '{ACCESS_TOKEN_KEY}',
+    accessTokenSecret: '{ACCESS_TOKEN_SECRET}'
+};
